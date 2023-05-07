2f854c6ad3b4c1523ce4620abc0de2056446db17
Kevin Özkaraca <80677798+kevinozkaraca@users.noreply.github.com>
2023-05-07 09:26:25 +0300

    ajout de fonciton filtre

--- fonctions/filtreDesInputsDeCouleur.js
+++ fonctions/filtreDesInputsDeCouleur.js
@@ -1,5 +1,54 @@
-function filtreDesInputs() {
-    console.log("first");
+// Description: Permet de filtrer les listes de chaque input
+function filtreDesInputsDeCouleur() {
+    const input = document.querySelectorAll(".selectionDeTag");
+    const list = document.querySelectorAll(".bloquesDeListe");
+    const appareilsChevron = document.getElementsByClassName("bouttonDeRecherche")[1];
+    const ustensilsChevron = document.getElementsByClassName("bouttonDeRecherche")[2];
+    const ingredientsChevron = document.getElementsByClassName("bouttonDeRecherche")[0];
+    input[0].addEventListener("keyup", (e) => {
+        const value = e.target.value.toLowerCase();
+        if (value.length == 3) {
+            ingredientsChevron.click();
+        }
+        list[0].querySelectorAll("li").forEach((li) => {
+            if (li.innerText.toLowerCase().includes(value)) {
+                li.style.display = "block";
+            } else {
+                li.style.display = "none";
+            }
+        });
+    }
+    );
+    input[1].addEventListener("keyup", (e) => {
+        const value = e.target.value.toLowerCase();
+        if (value.length == 3) {
+            appareilsChevron.click();
+        }
+        list[1].querySelectorAll("li").forEach((li) => {
+            if (li.innerText.toLowerCase().includes(value)) {
+                li.style.display = "block";
+            } else {
+                li.style.display = "none";
+            }
+        });
+    }
+    );
+    input[2].addEventListener("keyup", (e) => {
+        const value = e.target.value.toLowerCase();
+        if (value.length == 3) {
+            ustensilsChevron.click();
+        }
+        list[2].querySelectorAll("li").forEach((li) => {
+            if (li.innerText.toLowerCase().includes(value)) {
+                li.style.display = "block";
+            } else {
+                li.style.display = "none";
+            }
+        });
+    }
+    );
+
+
 }
 
-export default filtreDesInputs;
+export default filtreDesInputsDeCouleur;
