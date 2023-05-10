4552f61f99a78f5dfea6dc4808f6f294c0aaf21e
Kevin Özkaraca <80677798+kevinozkaraca@users.noreply.github.com>
2023-05-10 18:53:32 +0300

    ajout de fonciton

--- fonctions/filtreParTags.js
+++ fonctions/filtreParTags.js
@@ -5,21 +5,16 @@ function filtreParTags() {
     const motClefChoisi = document.querySelector("#motClefChoisi");
     let ensembleDesTags = [];
     let tag = document.querySelectorAll(".tag");
-    if (tag.length > 0) {
-        let ensembleDesTags = [...tag].map((tag) => tag.innerText);
-        // filtrer les recettes par tags
-        let recettesFiltrees = recipes.filter((recipe) => {
-            let ingredients = recipe.ingredients.map((ingredient) => ingredient.ingredient);
-            let ustensils = recipe.ustensils;
-            let appliance = recipe.appliance;
-            let tags = [...ingredients, ...ustensils, appliance];
-            return ensembleDesTags.every((tag) => tags.includes(tag));
-        }
+    let recettesFiltrees = [];
+    if (tag) {
+        for (let i = 0; i < tag.length; i++) {
+            ensembleDesTags.push(tag[i].innerText);
+            // filtre des recettes par l'ensemble des tags
 
-        );
-        affichageDesRecettes(recettesFiltrees);
-    } else {
-        affichageDesRecettes(recipes);
+        }
     }
+    console.log(recettesFiltrees);
+    console.log(ensembleDesTags);
 }
+
 export default filtreParTags;
